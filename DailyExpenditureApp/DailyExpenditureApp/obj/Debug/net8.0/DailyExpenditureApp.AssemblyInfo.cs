dc8172add22f0119ea49a107a868c3e80bce5c98
DageP <rzmdlu@gmail.com>
2024-10-15 13:13:43 +0200

    removed target element

--- DailyExpenditureApp/DailyExpenditureApp/obj/Debug/net8.0/DailyExpenditureApp.AssemblyInfo.cs
+++ DailyExpenditureApp/DailyExpenditureApp/obj/Debug/net8.0/DailyExpenditureApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyExpenditureApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670d7f42e91e6ebc1714797b55e12b451b0dcd96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7928ff65bec774f1604006f252f64f156d5a3b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyExpenditureApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyExpenditureApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
