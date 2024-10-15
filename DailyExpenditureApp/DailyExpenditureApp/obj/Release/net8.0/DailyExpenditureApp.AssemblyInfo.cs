2af9d93ad25326b7c24bd1acf60f4ce242c6ae5b
DageP <rzmdlu@gmail.com>
2024-10-15 12:20:23 +0200

    retarget back to 4.7.2

--- DailyExpenditureApp/DailyExpenditureApp/obj/Release/net8.0/DailyExpenditureApp.AssemblyInfo.cs
+++ DailyExpenditureApp/DailyExpenditureApp/obj/Release/net8.0/DailyExpenditureApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyExpenditureApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591c27272bc51ea2014fc8c195030ec110b52e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670d7f42e91e6ebc1714797b55e12b451b0dcd96")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyExpenditureApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyExpenditureApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
